2832aff0900af4049fd9eac4cf3a94ac05446573
Hank Shen <swh@admpub.com>
2022-01-10 14:40:51 +0800

    update

--- transcoder.go
+++ transcoder.go
@@ -14,6 +14,6 @@ type Transcoder interface {
 	OutputPipe(w io.WriteCloser, r io.ReadCloser) Transcoder
 	WithOptions(opts Options) Transcoder
 	WithAdditionalOptions(opts Options) Transcoder
-	WithContext(ctx *context.Context) Transcoder
+	WithContext(ctx context.Context) Transcoder
 	GetMetadata() (Metadata, error)
 }
